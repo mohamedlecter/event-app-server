bb1c639c27a5607fe383de8c09dfeac1ebac6b19
Mohamed Abubaker <67129352+mohamedlecter@users.noreply.github.com>
2025-06-04 09:46:08 +0300

    remove Rate limiting

--- server.js
+++ server.js
@@ -58,14 +58,6 @@ app.use(helmet({
   crossOriginResourcePolicy: { policy: "cross-origin" }
 }));
 
-// Rate limiting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again later.",
-});
-app.use(limiter);
-
 // Logging middleware
 app.use(morgan("combined"));
 
